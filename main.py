d1250494137d0870527ecdd7d09b3a65d97ad93a
Tikhon-code <markjonsonsigma@gmail.com>
2025-08-15 21:40:11 +0700

    Update main.py
    Added:
    Comments
    Float
    Shell mode
    Script mode

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
+import sys
 from sly import Lexer, Parser
 
-class shLexer(Lexer):
+class unicodingLexer(Lexer):
     # Set of token names.   This is always required
-    tokens = { COMMAND, NUMBER, COMMA, LBRACE, RBRACE, COLON, STRING, IDENTIFIER, NONE}
-    literals = {'=', '+', '-', '*', '/', '(', ')', '{', '}', ':', ',', '|'}
+    tokens = { COMMAND, INT, FLOAT, COMMA, LBRACE, RBRACE, COLON, STRING, IDENTIFIER}
     # String containing ignored characters between tokens
     ignore = ' \t'
+    ignore_comment = r'\#.*'
 
     # Regular expression rules for tokens
     COMMAND = r'\([a-zA-Z_][a-zA-Z0-9_]*\)'
-    NUMBER  = r'\d+'
+    INT     = r'\d+'
+    FLOAT   = r'\d+\.\d+'
     LBRACE  = r'\{'
     RBRACE  = r'\}'
-    COLON   = r':'
-    COMMA   = r','
+    COLON   = r'\:'
+    COMMA   = r'\,'
     STRING  = r'\".*?\"'
     IDENTIFIER  = r'[a-zA-Z_][]a-zA-Z0-9_]*'
-    
-class shParser(Parser):
-    tokens = shLexer.tokens
+
+class unicodingParser(Parser):
+    tokens = unicodingLexer.tokens
+
+    def error(self, p):
+        if p != None:
+            if p:
+                print(f"Error {p.type, p.value}")
+            else:
+                print("Error")
+
 
     @_('COMMAND COLON block')
     def statement(self, p):
@@ -40,31 +50,57 @@ class shParser(Parser):
     def arg(self, p):
         return p.STRING[1:-1]
         
-    @_('NUMBER')
+    @_('INT')
     def arg(self, p):
-        return int(p.NUMBER)
+        return int(p.INT)
         
+    @_('FLOAT')
+    def arg(self, p):
+        return float(p.FLOAT)
+    
     @_('IDENTIFIER')
     def arg(self, p):
         return p.IDENTIFIER
     
 def interpretator(ast):
-    commands = ["(print)"]
+    commands = ["(print)", "(var)"]
     if ast is not None and ast[1] in commands:
         command, name, content = ast
         cmd_name = name[1:-1]
     
         if cmd_name == 'print':
             print(*content)
-        else:
-            print(f"{ast[1]} is not system command")
-    else:
-        print("Err")
-if __name__ == '__main__':
-    lexer = shLexer()
-    parser = shParser()
+        if cmd_name == 'var':
+            eval(*content)
+            
+def shell_mode():
+    lexer = unicodingLexer()
+    parser = unicodingParser()
     while True:
         data = input(">>> ")
     
+        if data == "(exit)":
+            break
         ast = parser.parse(lexer.tokenize(data))
         interpretator(ast)
+
+def script_mode():
+    lexer = unicodingLexer()
+    parser = unicodingParser()
+    if len(sys.argv) > 2:
+        num_line = 0
+        with open(sys.argv[2], 'r') as f:
+            for line in f:
+                num_line += 1
+                try:
+                    ast = parser.parse(lexer.tokenize(line.rstrip('\n')))
+                    interpretator(ast)
+                except Exception as e:
+                    print(f"Error in {num_line} line: {e}")
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "shell":
+            shell_mode()
+        if sys.argv[1] == "run":
+            script_mode()
+    
